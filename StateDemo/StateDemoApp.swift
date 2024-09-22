1f54e70987cb6d3f1186ca823f372875b86bb564
David Rocharz <davidrocharz@gmail.com>
2024-09-22 18:00:32 -0400

    Initial Commit

---
+++ StateDemo/StateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateDemoApp.swift
+//  StateDemo
+//
+//  Created by David Rocharz on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct StateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
